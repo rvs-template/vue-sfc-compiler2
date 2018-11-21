4212d79721fdf15e53dcd642131b4c0558a20a94
hitao123 <hitao1234@gmail.com>
2018-11-21 22:08:15 +0800

    add babel template

---
+++ src/template.js
@@ -0,0 +1,21 @@
+/**
+ * parse html to render function
+ */
+
+import * as compiler from 'vue-template-compiler';
+import compilerES2015 from 'vue-template-es2015-compiler';
+
+const defaultConfig = {
+  preserveWhitespace: false
+};
+
+export default function parse(template, config = {}) {
+  const compiled = compiler.compile(template.content, {
+    ...defaultConfig,
+    ...config
+  });
+
+  return {
+    
+  }
+}
